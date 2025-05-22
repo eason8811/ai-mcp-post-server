4fd2c6dfc19a5285afdd3ad8034380458a842605
Eason <3220563569@qq.com>
2025-05-22 22:32:54 +0800

    项目初始化

---
+++ src/main/java/xin/eason/aimcppostserver/AiMcpPostServerApplication.java
@@ -0,0 +1,13 @@
+package xin.eason.aimcppostserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMcpPostServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiMcpPostServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xin/eason/aimcppostserver/AiMcpPostServerApplicationTests.java
@@ -0,0 +1,13 @@
+package xin.eason.aimcppostserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiMcpPostServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
